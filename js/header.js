af54be656a277973f47fa7d9918a103b2a8bcfb3
playmidia <diego@playmidiapublicidade.com.br>
2019-02-13 15:19:17 -0200

    inserido botão ir ao top

--- js/header.js
+++ js/header.js
@@ -1,7 +1,9 @@
+let body = document.querySelector('body');
 let nav = document.querySelector('nav');
 let navAncoras = document.querySelectorAll('nav a');
 let menu = document.querySelector('.menu');
 let lines = document.querySelectorAll('.line');
+let arrowUp = document.querySelector('.top');
 
 menu.onclick = () => {
     nav.classList.toggle('nav-toggle');
@@ -10,5 +12,15 @@ menu.onclick = () => {
     }
     for(let navAncora of navAncoras){
     navAncora.classList.toggle('nav-a-toggle');
+    }
+}
+
+arrowUp.onclick = () => scrollTo(0, 0);
+
+body.onscroll = () => {
+    if(scrollY > 243){
+        arrowUp.classList.add('top-visible');
+    }else{
+        arrowUp.classList.remove('top-visible');
     }
 }
--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,11 @@
 let arrow = document.querySelector('.arrow-down');
+let scrollText = document.querySelector('.section-one span');
+let scrolldown = document.querySelector('.scroll-down');
 
-arrow.onclick = () => location.href = "#section-two";
+function scroll() {
+    location.href = "#section-two";
+}
+
+arrow.onclick = () => scroll();
+scrollText.onclick = () => scroll();
+scrolldown.onclick = () => scroll();
